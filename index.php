c23ca1aed0e69b620a14ccdc04131925b0d22fd8
windhamdavid <david@davidawindham.com>
2014-08-09 16:05:04 -0400

    up

--- index.php
+++ index.php
@@ -33,39 +33,44 @@
 	</div>
 	
 	<div id="intro2" data-1400="display:block;" data-4100="display:none;">
-		<div class="intro_ease" data-1400="left:-30%" data-2100="left:93%;" data-3100="left:93%;" data-4100="left:150%;">
+		<div class="intro_ease" data-1400="left:-30%" data-2100="left:93%;" data-3100="left:93%;" data-4100="left:150%;" data-3100="background-image:!url(img/dress.png);" data-4100="background-image:!url(img/dress.png)">
+			<img src="wp-content/themes/bml/img/bells.png">
 			<h4 class="vibes">No need to dress up, no need to wrap gifts.</h4>
 			<h4 class="vibes">Just stay at home, and send us a good wish.</h4>
 			<h4 class="vibes">We appreciate your love and cheer.</h4>
 			<h4 class="vibes">But we're off on our journey, have no fear.</h4>
+			<img src="wp-content/themes/bml/img/dress.png">	
 		</div>
+
+			
+
 	</div>
 	
-	<div id="story_wrap" data-3200="top:200%" data-4400="top:0%;" data-11000="top:0%;" data-12200="top:-100%;" data-3400="background:rgba(0,0,0,0);" data-3700="background:rgba(104,130,91,.25);">
+	<div id="story_wrap" data-3200="top:200%" data-4400="top:0%;" data-11000="top:0%;" data-12200="top:-100%;"   data-3400="background:rgba(0,0,0,0);" data-3700="background:rgba(104,130,91,.25);">
 		<div id="story">
-			<div class="story_ease" data-3400="top:100%" data-4400="top:5%;" data-8000="top:5%;" data-9000="top:-35%;">
+			<div class="story" data-3400="top:100%" data-4400="top:5%;" data-8000="top:5%;" data-9000="top:-35%;">
 					<h4 class="vibes">About 10 years ago<br /> a guy named Dave met a girl named Ginny.<br />
 					Soon after, the two would take a trip to New York City.</h4>
 			</div>
-			<div class="story_ease" data-4000="top:100%" data-5000="top:20%;" data-8000="top:20%;" data-9000="top:-35%;">
+			<div class="story" data-4000="top:100%" data-5000="top:20%;" data-8000="top:20%;" data-9000="top:-35%;">
 					<h4 class="vibes">Days and weeks and many more,<br />
 					The couple found love they could not ignore.<br />
 					They dwelled in cities, the trees, and farm,<br />
 					Each would have its newfound charm.</h4>
 			</div>
-			<div class="story_ease" data-5000="top:100%;" data-6000="top:40%;" data-8000="top:40%;" data-9000="top:-40%;">	
+			<div class="story" data-5000="top:100%;" data-6000="top:40%;" data-8000="top:40%;" data-9000="top:-40%;">	
 					<h4 class="vibes">And then one day came when they decided to marry.<br />
 					They had made it 10 years, after all, it’s not so scary.<br />
 					Money can’t buy love, this they knew.<br />
 					But it sure could pay for a trip, to start anew.</h4>
 			</div>
-			<div class="story_ease" data-6000="top:100%;" data-7000="top:60%;" data-8000="top:60%;" data-9000="top:-40%;">
+			<div class="story" data-6000="top:100%;" data-7000="top:60%;" data-8000="top:60%;" data-9000="top:-40%;">
 					<h4 class="vibes">So tip your hat to these big-hearted.<br />
 					Who are embarking on a journey to where it all started.<br />
 					To jumpstart another decade, with its ups and downs.<br />
 					New experiences and perhaps new towns.</h4>
 			</div>
-			<div class="story_ease" data-8000="top:100%" data-9000="top:15%;" data-10000="top:15%;" data-11000="top:-40%;">
+			<div class="story" data-8000="top:100%" data-9000="top:15%;" data-10000="top:15%;" data-11000="top:-40%;">
 					<div id="bmlcount"></div><div id="heart">&nbsp;</div>
 					<h4 class="vibes">But have no worries for this one of many couples.<br />
 					Because they’re a good team and can tackle life’s troubles.<br />
@@ -84,15 +89,17 @@
 	</div>
 
 <?php wp_footer(); ?>
+
 <script>
-var s = skrollr.init({
-	edgeStrategy: 'set',
-	easing: {
-		inverted: function(p) {
-			return 1-p;
-		}
-	}
-});
+var s = skrollr.init({edgeStrategy: 'set', easing: {inverted: function(p) {return 1-p;}} });
+	skrollr.menu.init(s), {
+	    animate: true,
+	    easing: 'sqrt',
+	    scale: 2,
+	    duration: function(currentTop, targetTop) {
+	        return 500;
+	    },
+	    }
 </script>
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){

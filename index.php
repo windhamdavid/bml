90d2505b175a69266924de393b42b05f8406808c
windhamdavid <david@davidawindham.com>
2014-08-09 09:18:20 -0400

    final countdown

--- index.php
+++ index.php
@@ -16,8 +16,8 @@
 <body>
 	
 	<div id="bg1" data-0="display:none;" data-1000="display:block;background-position:0px 0px;" data-end="background-position:-500px -10000px;"></div>
-	<div id="bg2" data-0="display:none;" data-8000="display:block;background-position:0px 0px;" data-end="background-position:-500px -8000px;"></div>
-	<div id="bg3" data-0="display:none;" data-20000="display:block;background-position:0px 0px;" data-end="background-position:-500px -6000px;"></div>
+	<div id="bg2" data-0="display:none;" data-4000="display:block;background-position:0px 0px;" data-end="background-position:-500px -8000px;"></div>
+	<div id="bg3" data-0="display:none;" data-8000="display:block;background-position:0px 0px;" data-end="background-position:-500px -6000px;"></div>
 	<div id="progress" data-0="width:0%;background:rgb(130, 104, 130);" data-end="width:100%;background:rgb(103, 130, 91);"></div>
 	
 	<div id="intro" data-0="display:block;" data-1400="display:none;">
@@ -32,8 +32,8 @@
 		</div>
 	</div>
 	
-	<div id="intro2" data-0="display:none;" data-1400="display:block;" data-4000="display:none;">
-		<div class="intro_ease" data-1400="left:0%" data-2100="left:93%;">
+	<div id="intro2" data-1400="display:block;" data-4100="display:none;">
+		<div class="intro_ease" data-1400="left:0%" data-2100="left:93%;" data-3100="left:93%;" data-4100="left:150%;">
 			<h4 class="vibes">No need to dress up, no need to wrap gifts.</h4>
 			<h4 class="vibes">Just stay at home, and send us a good wish.</h4>
 			<h4 class="vibes">We appreciate your love and cheer.</h4>
@@ -41,31 +41,31 @@
 		</div>
 	</div>
 	
-	<div id="story_wrap" data-3400="background:rgba(0,0,0,0);" data-8000="background:rgba(104,130,91,.25);">
+	<div id="story_wrap" data-3200="top:200%" data-4400="top:0%;" data-11000="top:0%;" data-12200="top:-100%;" data-3400="background:rgba(0,0,0,0);" data-3700="background:rgba(104,130,91,.25);">
 		<div id="story">
-			<div class="story_ease" data-3400="top:100%" data-4400="top:20%;" data-6400="top:20%;" data-7400="top:-35%;">
+			<div class="story_ease" data-3400="top:100%" data-4400="top:5%;" data-8000="top:5%;" data-9000="top:-35%;">
 					<h4 class="beanie">About 10 years ago<br /> a guy named Dave met a girl named Ginny.<br />
 					Soon after, the two would take a trip to New York City.</h4>
 			</div>
-			<div class="story_ease" data-4000="top:100%" data-5000="top:45%;" data-6400="top:45%;" data-7400="top:-35%;">
+			<div class="story_ease" data-4000="top:100%" data-5000="top:20%;" data-8000="top:20%;" data-9000="top:-35%;">
 					<h4 class="beanie">Days and weeks and many more,<br />
 					The couple found love they could not ignore.<br />
 					They dwelled in cities, the trees, and farm,<br />
 					Each would have its newfound charm.</h4>
 			</div>
-			<div class="story_ease" data-7000="top:100%;" data-8000="top:15%;" data-10000="top:15%;" data-15000="top:-40%;">	
+			<div class="story_ease" data-5000="top:100%;" data-6000="top:40%;" data-8000="top:40%;" data-9000="top:-40%;">	
 					<h4 class="beanie">And then one day came when they decided to marry.<br />
 					They had made it 10 years, after all, it’s not so scary.<br />
 					Money can’t buy love, this they knew.<br />
 					But it sure could pay for a trip, to start anew.</h4>
 			</div>
-			<div class="story_ease" data-8000="top:100%;" data-9000="top:50%;" data-10000="top:50%;" data-15000="top:-40%;">
+			<div class="story_ease" data-6000="top:100%;" data-7000="top:60%;" data-8000="top:60%;" data-9000="top:-40%;">
 					<h4 class="beanie">So tip your hat to these big-hearted.<br />
 					Who are embarking on a journey to where it all started.<br />
 					To jumpstart another decade, with its ups and downs.<br />
 					New experiences and perhaps new towns.</h4>
 			</div>
-			<div class="story_ease" data-12000="top:100%" data-14000="top:35%;" data-15000="top:35%;" data-20000="top:-40%;">
+			<div class="story_ease" data-8000="top:100%" data-9000="top:35%;" data-10000="top:35%;" data-11000="top:-40%;">
 					<h4 class="beanie">But have no worries for this one of many couples.<br />
 					Because they’re a good team and can tackle life’s troubles.<br />
 					For it is their love that will make them last.<br />
@@ -74,70 +74,16 @@
 		</div>
 	</div>
 	
-	<div id="trip" data-20000="top:-150%" data-22000="top:0%;" data-28000="top:-100%;" data-20000="background:rgba(0,0,0,0);" data-27000="background:rgba(130,104,130,.5);">
-		<div class="easing" data-20000="bottom:-250%" data-22000="bottom:0%;">
-				<div id="gform">
+	<div id="trip" data-11000="top:-200%" data-12000="top:0%;" data-13000="top:0%;" data-14000="top:-100%;" data-9500="background:rgba(130,104,130,.5);" data-12000="background:rgba(130,104,130,.5);">
+		<div class="easing">
+			<div id="bmlcount"></div>
+			<div id="gform">
 				<?php gravity_form(1, false, false, false, '', true); ?>
-				</div>
 				<div id="heart">&nbsp;</div>
-				<div id="bmlcount">
-					<h3>September 27<sup>th</sup>, &nbsp;<span id="year">2014</span>.</h3>	
-				</div>
-				
 				<div class="buy-love">
 					<a id="inline" href="#buy-love"><button>Buy Some Love</button></a>
 				</div>
-		</div>
-	</div>
-	
-	<div style="display:none">
-		<div id="buy-love">
-			<div id="wp-stripe-wrap">
-				<form id="wp-stripe-payment-form">
-				<input type="hidden" name="action" value="wp_stripe_charge_initiate" />
-				<input type="hidden" name="nonce" value="bd39ddb4d5" />
-				<div class="wp-stripe-details">
-				    <div class="wp-stripe-notification wp-stripe-failure payment-errors" style="display:none"></div>
-				    <div class="stripe-row"><input type="text" name="wp_stripe_name" class="wp-stripe-name" placeholder="Name *" autofocus required /></div>
-				    <div class="stripe-row"><input type="email" name="wp_stripe_email" class="wp-stripe-email" placeholder="E-mail" /></div>
-				    <div class="stripe-row"><textarea name="wp_stripe_comment" class="wp-stripe-comment" placeholder="Comments?"></textarea></div>
-				</div>
-				<div class="wp-stripe-card">
-				    <div class="stripe-row"><input type="text" name="wp_stripe_amount" autocomplete="off" class="wp-stripe-card-amount" id="wp-stripe-card-amount" placeholder="Amount (USD) *" required /></div>
-				    <div class="stripe-row"><input type="text" autocomplete="off" class="card-number" placeholder="Card Number *" required /></div>
-				    <div class="stripe-row">
-				        <div class="stripe-row-left"><input type="text" autocomplete="off" class="card-cvc" placeholder="CVC Number *" maxlength="4" required /></div>
-				        <div class="stripe-row-right">
-				            <span class="stripe-expiry">EXP</span>
-				            <select class="card-expiry-month">
-				                <option value="1">01</option>
-				                <option value="2">02</option>
-				                <option value="3">03</option>
-				                <option value="4">04</option>
-				                <option value="5">05</option>
-				                <option value="6">06</option>
-				                <option value="7">07</option>
-				                <option value="8">08</option>
-				                <option value="9">09</option>
-				                <option value="10">10</option>
-				                <option value="11">11</option>
-				                <option value="12">12</option>
-				            </select>
-				            <span></span>
-				            <select class="card-expiry-year">
-				            <option value="2014">2014</option><option value="2015">2015</option><option value="2016">2016</option><option value="2017">2017</option><option value="2018">2018</option><option value="2019">2019</option>
-							</select>
-				        </div>
-				    </div>
-				    </div>
-				    <div class="stripe-row">
-				        <input type="checkbox" name="wp_stripe_public" value="public" checked="checked" /> <label class="public">share your comments on our event page
-				    </div>
-				    <div style="clear:both"></div>
-				    <input type="hidden" name="wp_stripe_form" value="1"/>
-				    <button type="submit" class="stripe-submit-button">Buy</button>
-				    <div class="stripe-spinner"></div>
-				</form>
+				
 			</div>
 		</div>
 	</div>
@@ -153,6 +99,7 @@ var s = skrollr.init({
 	}
 });
 </script>
+
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
--- js/init.js
+++ js/init.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+jQuery(document).ready(function ($) {
 	$("#bmlcount")
 	.countdown("2014/09/27", function(event) {
 		 $(this).text(event.strftime('%D days %H:%M:%S'));

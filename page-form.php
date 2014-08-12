cb6435666104e34578f84ab8aae36bb508f37a38
windhamdavid <david@davidawindham.com>
2014-08-12 19:46:03 -0400

    form

--- page-form.php
+++ page-form.php
@@ -12,16 +12,14 @@
 <div class="container">
 	<div class="column sixteen">
 		<div class="form_header">
-			<h5 class="vibes">David A. Windham + Ginny L. Gast</h5>
+			<h5 class="vibes">&#10087; &nbsp; David A. Windham&nbsp;  &amp; &nbsp; Ginny L. Gast &nbsp; &#10087;</h5>
 			<h5 class="cardo">The Twenty-Seventh Of September, Two Thousand Fourteen</h4><br />
 			<img src="../wp-content/themes/bml/img/swirl.png" width="100">
+			
 		</div>
-	<div id="gform">
-		<?php gravity_form(1, false, false, false, '', true); ?>
-	</div>
-	<div class="form_credit">
-		This page is encrypted and NO credit card information is stored
-		<img src="../wp-content/themes/bml/img/stripe.png" width="100">
+		<div id="gform">
+			<?php gravity_form(1, false, false, false, '', true); ?>
+		</div>
 	</div>
 </div>
 <?php wp_footer(); ?>

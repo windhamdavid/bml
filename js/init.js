57280a005340817a9ce7c15089e96e6c9e32d1ef
windhamdavid <david@davidawindham.com>
2014-09-04 13:16:44 -0400

    touch

--- js/init.js
+++ js/init.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function ($) {
 	.countdown("2014/09/27 09:27:00", function(event) {
 		 $(this).text(event.strftime('in %D days %H:%M:%S'));
 	}); 
-    $('a.wish').on('click touchend', function() { 
+    $('a').on('click touchend', function() { 
         var link = $(this).attr('href');   
         window.open(link,'_self'); 
         return false;
